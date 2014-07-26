0216788e0a842c4e8927dbcdb83a5024c9d755f1
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-07-26 18:07:02 +0600

    Added some debug output

--- example/baseline_rng.js
+++ example/baseline_rng.js
@@ -1,12 +1,14 @@
 /* global CONS */
 function main(world, state) {
+	state = state + 1;
+
     function mod (a, b) {
         return a - (a / b) * b;
     }
 
-    function step (w, i) {
-    	var c = mod(i, 4);
-    	return CONS(i + 1, c);
+    function step (world, state) {
+    	var c = mod(state, 4);
+    	return CONS(state + 1, c);
     }
 
     return CONS(42, step);
